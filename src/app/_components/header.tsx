3d4d7e886de9e1e4deadb63159770169ba43db5d
Jared588 <jaredeprice@gmail.com>
2024-09-05 23:09:55 +0200

    Change Heading font

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { getServerAuthSession } from "~/server/auth";
 import { type Session } from "next-auth";
 import Image from "next/image";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
 interface HeaderProps {
   session: Session | null;
@@ -11,7 +14,7 @@ interface HeaderProps {
 export default async function Header({ session }: HeaderProps) {
   return (
     <header className="h-15 flex w-full justify-between border-b-2 border-slate-400">
-      <Link href="/" className="p-5 text-3xl text-zinc-200">T3-Messaging!</Link>
+      <Link href="/" className={`p-5 text-3xl text-zinc-200 ${roboto.className}`}>T3-Messaging!</Link>
       <div className="flex content-center items-center p-5 text-xl text-zinc-200">
         {session?.user.image ? (
           <Link href="/profile" className="mr-5">
@@ -25,7 +28,7 @@ export default async function Header({ session }: HeaderProps) {
             />
           </Link>
         ) : null}
-        <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+        <Link href={session ? "/api/auth/signout" : "/api/auth/signin"} className={`${roboto.className}`}>
           {session ? "Sign out" : "Sign in"}
         </Link>
       </div>
